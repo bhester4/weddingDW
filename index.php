4d34dbe5742d1135af36234a427aee3b3ccf7c74
unknown <Meg@.(none)>
2012-08-06 23:39:51 -0500

    Playing around with background and added some images

--- index.php
+++ index.php
@@ -3,3 +3,13 @@
 echo "this is a Gallifrey";
   
 ?>
+
+<html>
+	<head>
+		Wibbly Wobbly Wedding
+		<link rel="stylesheet" type="text/css" href="css/splash.css"></style>
+	</head>
+	<body>
+		<h3>Tardis</h3>
+	</body>
+</html>

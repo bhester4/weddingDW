8c710d400d43a79e77b8cb5f565b6d42052cb4c3
bhester4 <bhester4@murraystate.edu>
2012-08-06 23:51:43 -0500

    skelton

--- index.php
+++ index.php
@@ -1,5 +1,83 @@
 <?php
 
-echo "this is a Gallifrey";
+//Futreu spot for if statements to guide page and set varibles
   
 ?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!-- main digicrits page copyright 2012-->
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Wedding Site</title>
+
+
+
+<?php //include headerfile ?>
+
+</head>
+
+<body>
+
+
+
+<?php 
+
+//determine whether to show logged out or logged in nav
+session_start();
+if($_SESSION['login'] == 1) {
+	
+	
+	
+} else {
+	
+	
+}
+
+//include "layout/" . $nav; ?>
+
+
+
+
+<div class="container">
+
+
+
+<?php 
+
+
+
+
+
+//include "layout/" . $header;
+
+
+
+
+?>
+
+
+
+<!-- end header -->
+
+
+  <div class="content">
+  
+  
+  <?php //include "layout/" . $content;?>
+  
+  
+  
+  <!-- end .content --></div>
+  <div class="clearfooter"></div>
+<!-- end .container --></div>
+
+  <?php //include"layout/footer.php"; ?>
+
+
+</body>
+</html>

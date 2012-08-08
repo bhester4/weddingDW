7bde8e0cf2fe75b7b0bdabda9ce161227288f00d
winterwolf07 <Meg@.(none)>
2012-08-08 13:05:16 -0500

    Gallery looks awesome. Currently no way to turn off auto-play, but whatever. :P

--- index.php
+++ index.php
@@ -22,8 +22,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-<!-- main digicrits page copyright 2012-->
-
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="viewport" content="width=device-width" />
--- layout/galleryTest.php
+++ layout/galleryTest.php
@@ -1,21 +1,6 @@
 <html>
 	<head>
 		<title>Gallery Test</title>
-		<!--<link type="text/css" src="../css/jquery.galleryview-3.0-dev.css" />
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
-		<script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
-		<script type="text/javascript" src="../js/jquery.timers-1.2.js"></script>
-		<script type="text/javascript" src="../js/jquery.galleryview-3.0-dev.js"></script>
-		<script>
-			$(function(){
-				$('#Gatlinburg').galleryView({
-					panel_width: 800,
-					panel_height: 600,
-					frame_width: 120,
-					frame_height: 90
-				});
-			});
-		</script>-->
 		
 		<!-- First, add jQuery (and jQuery UI if using custom easing or animation -->
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
@@ -24,6 +9,7 @@
 		<!-- Second, add the Timer and Easing plugins -->
 		<script type="text/javascript" src="../js/jquery.timers-1.2.js"></script>
 		<script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
+		<script type="text/javascript" src="../js/jquery.batchImageLoad.js"></script>
 		
 		<!-- Third, add the GalleryView Javascript and CSS files -->
 		<script type="text/javascript" src="../js/jquery.galleryview-3.0-dev.js"></script>
@@ -31,9 +17,35 @@
 		
 		<!-- Lastly, call the galleryView() function on your unordered list(s) -->
 		<script type="text/javascript">
+			//function loadImages() {	
 			$(function(){
-				$('#Gatlinburg').galleryView();
+				// IMPORTANT: This is how you use the plugin. Apply it on a
+				// list of img tags and provide callback function.
+				$('#Gatlinburg').find('img').batchImageLoad({
+					loadingCompleteCallback: function(){
+						$('#Gatlinburg').galleryView({
+							transition_interval: 3000,
+							panel_width: 600,
+							panel_height: 450,
+							panel_animation: 'crossfade',
+							panel_scale: 'fit',
+							show_filmstrip: false,
+							autoplay: true,
+							show_infobar: false
+						});						
+					}
+					//If you want to do something when EACH image is loaded, replace "imageLoaded" with your function name.
+					//Don't change "imageLoadedCallback"
+					//imageLoadedCallback: imageLoaded
+				});
 			});
+			/*
+			$(function(){
+				$('#Gatlinburg').galleryView({
+					transition_speed: 600,
+					panel_animation: 'slide'
+				});
+			});*/
 		</script>
 
 
@@ -46,36 +58,11 @@
 				$photoList .= "<li><img src='../images/Gatlinburg Meg and Ben/Gat-Meg-Ben-" . $i . ".jpg' /></li>";
 			}
 			$photoList .= "</ul>";
-			
+		?>
+		<h1>Photo Gallery Test</h1>
+		<h3>Meg and Ben in Gatlinburg</h3>
+		<?php
 			echo $photoList;
 		?>
-		
-		<!--
-		<div class="gv_galleryWrap" style="width: 800px; height: 445px; padding: 5px; ">
-			<div class="gv_gallery" style="width: 800px; height: 445px; ">
-				<div class="gv_panelWrap" style="width: 800px; height: 400px; top: 0px; left: 0px; ">
-					<div class="gv_panel" style="width: 800px; height: 400px; ">
-						<img style="visibility: visible; width: 800px; height: 531.7171717171717px; top: -66px; left: 0px; " src="img/photos/bp1.jpg">
-					</div>
-					<div class="gv_panelNavNext" style="top: 190px; right: 10px; display: none; "></div>
-					<div class="gv_panelNavPrev" style="top: 190px; left: 10px; display: none; "></div>
-					<div class="gv_infobar" style="display: none; opacity: 1; ">1 of 13</div>
-				</div>
-				<div class="gv_filmstripWrap" style="width: 680px; height: 40px; bottom: 0px; left: 0px; ">
-					<div class="gv_filmstrip" style="width: 3315px; height: 40px; left: -1105px; ">
-						<div class="gv_frame" style="width: 80px; height: 40px; margin-right: 5px; margin-bottom: 5px; ">
-							<div class="gv_thumbnail current" style="width: 80px; height: 40px; opacity: 1; ">
-								<img style="visibility: visible; width: 80px; height: 53.17171717171718px; top: -7px; left: 0px;" src="img/photos/bp1.jpg">
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="gv_navWrap" style="width: 70px; height: 30px; bottom: 5px; right: 0px; ">
-					<div class="gv_navPrev" style=""></div>
-					<div class="gv_navPlay" style=""></div>
-					<div class="gv_navNext" style=""></div>
-				</div>
-			</div>
-		</div>-->
 	</body>
 </html>
